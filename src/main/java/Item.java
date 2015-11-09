e06b6a4619d1bde898d39320a113af7fbf39650a
Alexander Nohe <alex.nohe427@gmail.com>
2015-11-09 09:30:17 -0500

    Added notes

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -2,7 +2,10 @@
  * Created by alexandernohe on 11/7/15.
  */
 
-//Not yet fleshed out.  Added so that way the player class can be built a little more.
+// Not yet fleshed out.  Added so that way the player class can be built a little more.
+// This would probably be better done as an interface with a name and some sort of action
+// And then we can implement some sort of action as well and implement it in classes
+
 public class Item {
     private String name;
 }
